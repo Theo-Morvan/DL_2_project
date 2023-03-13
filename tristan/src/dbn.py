abc7e2fae217cda08d5d8775031465ab3ede2486
Windu12345 <32morvantheo@gmail.com>
2023-03-13 09:36:17 +0100

    latest modifications Theo

--- tristan/src/dbn.py
+++ tristan/src/dbn.py
@@ -1,4 +1,5 @@
 from src.rbm import RBM
+import numpy as np
 
 class DBN:
     def __init__(self, sizes):
@@ -11,8 +12,15 @@ class DBN:
             rbm.train(X, lr, batch_size, nb_epochs)
             X = rbm.entree_sortie(X)
 
-    def generer_image(self, nb_iter, nb_images):
-        V = self.rbms[-1].generer_image(nb_iter, nb_images)
-        for rbm in reversed(self.rbms[:-1]):
-            V = rbm.sortie_entree(V)
-        return V
+    # def generer_image(self, nb_iter, nb_images):
+    #     V = self.rbms[-1].generer_image(nb_iter, nb_images)
+    #     for rbm in reversed(self.rbms[:-1]):
+    #         V = rbm.sortie_entree(V)
+    #     return V
+    
+    def generer_image(self, nb_iters_Gibbs, nb_images):
+        for j in range(nb_images):
+            v = self.rbms[-1].generer_image(nb_iters_Gibbs, nb_images)
+            for i, rbm in enumerate(reversed(self.rbms[:-1])):
+                v = ((np.random.rand(rbm.input_size)<rbm.sortie_entree(v))*1)
+        return v
--- tristan/src/rbm.py
+++ tristan/src/rbm.py
@@ -19,23 +19,38 @@ class RBM:
     def train(self, X, lr, batch_size, nb_epochs):
         for epoch in range(nb_epochs):
             for i in range(0, len(X), batch_size):
+                X_batch = X[i:min(i + batch_size, X.shape[0])].copy()
+                t_b = X_batch.shape[0]
                 v_0 = X[i:min(i + batch_size, X.shape[0])]
-                h_0 = self.entree_sortie(v_0)
-                v_1 = self.sortie_entree(h_0)
+                p_h_v_0 = self.entree_sortie(v_0)
+                h_0 = (np.random.rand(t_b,self.output_size)<p_h_v_0)*1
+                p_v_h_0 = self.sortie_entree(h_0)
+                v_1 = (np.random.rand(t_b,self.input_size)<p_v_h_0)*1
                 h_1 = self.entree_sortie(v_1)
-                
-                grad_a = np.mean(v_0 - v_1, axis=0)
-                grad_b = np.mean(h_0 - h_1, axis=0)
-                grad_W = v_0.T @ h_0 - v_1.T @ h_1
+                p_h_v_1 = self.entree_sortie(v_1)
+
+                grad_a = np.sum(v_0 - v_1, axis=0)
+                grad_b = np.sum(p_h_v_0 - p_h_v_1, axis=0)
+                grad_W = v_0.T @ p_h_v_0 - v_1.T @ p_h_v_1
 
                 self.a += lr / len(v_0) * grad_a
                 self.b += lr / len(v_0) * grad_b
                 self.W += lr / len(v_0) * grad_W
             print("Epoch %d, erreur quadratique moyenne de reconstruction : %f" % (epoch, np.mean((X - self.sortie_entree(self.entree_sortie(X)))**2)))
 
-    def generer_image(self, nb_iter, nb_images):
-        H = np.random.binomial(1, 0.5, (nb_images, self.output_size))
-        for i in range(nb_iter):
-            V = self.sortie_entree(H)
-            H = self.entree_sortie(V)
-        return V
+    # def generer_image(self, nb_iter, nb_images):
+    #     H = np.random.binomial(1, 0.5, (nb_images, self.output_size))
+    #     for i in range(nb_iter):
+    #         V = self.sortie_entree(H)
+    #         H = self.entree_sortie(V)
+    #     return V
+    
+    def generer_image(self, nb_iters_Gibbs, nb_images):
+        images = np.zeros((nb_images, self.input_size))
+        for i in range(nb_images):
+            v = ((np.random.rand(self.input_size)<1/2)*1)     
+            for iter_gibb in range(nb_iters_Gibbs):
+                h = ((np.random.rand(self.output_size)<self.entree_sortie(v))*1)
+                v = ((np.random.rand(self.input_size)<self.sortie_entree(h))*1)
+            images[i,:] = v
+        return images
